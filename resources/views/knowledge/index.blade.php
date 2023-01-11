77c1d8b7c39b1656b37348e98533fcd09dbb7c45
Lucas Lucena <lucas.lucenak@gmail.com>
2023-01-10 23:15:14 -0300

    Add knowledge view

--- resources/views/knowledge/index.blade.php
+++ resources/views/knowledge/index.blade.php
@@ -1,1 +1,119 @@
-<h1>knowledge</h1>
+<section id="experience">
+    <h5>What Skills I Have</h5>
+    <h2>My Experience and Knowledge</h2>
+
+    <div className="container experience__container">
+
+        <div className="experience__backend">
+            <h3>Backend Development</h3>
+            <div className="experience__content">
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Java</h4>
+                        {/* <small className="text-light">2+ Years</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Spring Boot</h4>
+                        {/* <small className="text-light">1 Year</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Python</h4>
+                        {/* <small className="text-light">1 Year</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Selenium</h4>
+                        {/* <small className="text-light">1 Year</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>PostgreSQL</h4>
+                        {/* <small className="text-light">1 Year</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>MySQL</h4>
+                        {/* <small className="text-light">1 Year</small> */}
+                    </div>
+                </article>
+
+            </div>
+        </div>
+        {/* End of Backend Skills */}
+
+        <div className="experience__patterns">
+            <h3>Patterns Knowledge and Tools</h3>
+            <div className="experience__content">
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>SOLID</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>GoF</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>GRASP</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Git</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>GitHub</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+                <article className="experience__details">
+                    <BsPatchCheckFill className='experience__details-icon' />
+                    <div>
+                        <h4>Heroku</h4>
+                        {/* <small className="text-light">Experienced</small> */}
+                    </div>
+                </article>
+
+            </div>
+        </div>
+
+    </div>
+</section>
